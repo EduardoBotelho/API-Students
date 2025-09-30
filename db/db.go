50729922f9855af0f8e19611cdb7eec2e11c5c14
Eduardo Botelho <edu.botelho@yahoo.com>
2025-09-30 16:27:56 -0300

    Create dynamic student in DB

---
+++ db/db.go
@@ -0,0 +1,39 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Student struct {
+	gorm.Model
+	Name   string `json:"name"`
+	CPF    string `json:"cpf"`
+	Email  string `json:"email"`
+	Age    int    `json:"age"`
+	Active bool   `json:"active"`
+}
+
+func Init() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("student.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := db.AutoMigrate(&Student{}); err != nil {
+		log.Fatalln(err)
+	}
+	return db
+}
+
+func AddStudent(student Student) {
+	db := Init()
+	if result := db.Create(&student); result.Error != nil {
+		fmt.Println("Error creating student:", result.Error)
+		return
+	}
+	fmt.Println("Created student:", student.ID)
+}
